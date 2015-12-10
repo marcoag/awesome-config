ba17f7d581749ef96b22d6cdfea1b5047b9c8ae4
Marco A. Guiterrez <marcog@unex.es>
2015-12-10 15:20:45 +0800

    adjust in the tags

--- rc.lua
+++ rc.lua
@@ -94,8 +94,8 @@ end
 -- Define a tag table which hold all screen tags.
 
  tags = {
-    names  = { "term", "www", "robo", "files", "irc", "vps", "spoty", 8, 9 },
-    layout = { layouts[2], layouts[1], layouts[2], layouts[1], layouts[2], layouts[2], layouts[1], layouts[2], layouts[2] }
+    names  = { "term", "www", "robo", "files", "irc", "vps", "music", 8, 9 },
+    layout = { layouts[2], layouts[1], layouts[2], layouts[1], layouts[10], layouts[2], layouts[1], layouts[2], layouts[2] }
 }
 
 for s = 1, screen.count() do
